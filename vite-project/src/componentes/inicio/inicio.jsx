578a1af64f8346a8a9149f0712a372d9fe483ebf
LaraGuedes <lara.guedes569@gmail.com>
2024-06-05 20:15:14 -0300

    criando as rotas do projeto

---
+++ vite-project/src/componentes/inicio/inicio.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Inicio() {
+    return (
+        <>
+            <h1>
+                Olá eu sou o inicio
+
+            </h1>
+        </>
+
+    )
+}
+export default Inicio
